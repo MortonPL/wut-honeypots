5a3c51293a64ac6ec8e951b1304d8bcbed350d26
Dove6 <24943032+Dove6@users.noreply.github.com>
2023-05-28 22:58:16 +0200

    Add old kex compatibility option for interacting with kippo

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ HONEYPOT_STOP = {
     "cowrie": "docker stop auto-cowrie && docker rm auto-cowrie",
 }
 
-SSH_START = "ssh -T -o \"StrictHostKeyChecking=no\" -p 2222 root@127.0.0.1"
+SSH_START = "ssh -T -o \"StrictHostKeyChecking=no\" -o \"KexAlgorithms=+diffie-hellman-group1-sha1\" -p 2222 root@127.0.0.1"
 SSH_STOP = "exit"
 HONEYPOT_SSH_STOP = "exit"
 
