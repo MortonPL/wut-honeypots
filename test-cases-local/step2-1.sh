eb7accdcde630484df6ee7c10e5c0c5a313cb817
Dove6 <24943032+Dove6@users.noreply.github.com>
2023-05-29 03:40:03 +0200

    Add mounts for containers

--- test-cases-local/step2-1.sh
+++ test-cases-local/step2-1.sh
@@ -5,8 +5,8 @@
 
 ## Password-based authentication
 
-nmap -v -p "$PORT" --script ssh-brute --script-args userdb=usernames.lst,passdb=passwords.lst "$HOST"
+nmap -v -p "$PORT" --script +ssh-brute --script-args userdb=usernames.lst,passdb=passwords.lst "$HOST"
 
 ## Public key-based authentication
 
-nmap -v -p "$PORT" --script ssh-publickey-acceptance --script-args 'ssh.usernames={"jkaczmarski", "marcin001"}, publickeys={"./rsa1.pub", "./rsa2.pub"}' "$HOST"
+nmap -v -p "$PORT" --script +ssh-publickey-acceptance --script-args 'ssh.usernames={"jkaczmarski", "marcin001"}, publickeys={"./rsa1.pub", "./rsa2.pub"}' "$HOST"
